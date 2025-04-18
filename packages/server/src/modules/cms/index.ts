383c02aa9f891091d8dc43a91f1f751982b758c7
manegame <contact@manusnijhoff.nl>
2025-04-18 17:16:15 +0200

    feat: image generation

--- packages/server/src/modules/cms/index.ts
+++ packages/server/src/modules/cms/index.ts
@@ -1,66 +1,71 @@
-import { loadData } from "@modules/cms/sanity";
-import { queries } from "@modules/cms/groq";
-import type { ActivePrompts, Prompt } from "@sanity-types";
-import { client } from "./sanity";
+import { loadData } from "@modules/cms/sanity"
+import { queries } from "@modules/cms/groq"
+import type { ActivePrompts, Prompt } from "@sanity-types"
+import { client } from "./sanity"
 // import { createReadStream } from 'fs';
 
-// - - - - - - 
+// - - - - - -
 // READ
-// - - - - - - 
+// - - - - - -
 
 type ExpandedActivePrompts = ActivePrompts & {
-    activeEventPrompt: Prompt;
-    activeCorrectionPrompt: Prompt;
+  activeEventPrompt: Prompt
+  activeCorrectionPrompt: Prompt
 }
 
 export const getSystemPrompts = async () => {
-    const activePrompts = await loadData(queries.activePrompts, {}) as ExpandedActivePrompts;
-    return {
-        combinedSystemPrompt: combineSystemPrompts(activePrompts.activeEventPrompt),
-        correctionSystemPrompt: combineSystemPrompts(activePrompts.activeCorrectionPrompt)
-    };
+  const activePrompts = (await loadData(
+    queries.activePrompts,
+    {}
+  )) as ExpandedActivePrompts
+  return {
+    combinedSystemPrompt: combineSystemPrompts(activePrompts.activeEventPrompt),
+    correctionSystemPrompt: combineSystemPrompts(
+      activePrompts.activeCorrectionPrompt
+    ),
+  }
 }
 
 function combineSystemPrompts(doc: Prompt) {
-    return `Return format: ${doc.returnFormat?.code ?? ""} // ${doc.prompt ?? ""}`; 
+  return `Return format: ${doc.returnFormat?.code ?? ""} // ${doc.prompt ?? ""}`
 }
 
-// - - - - - - 
+// - - - - - -
 // WRITE
-// - - - - - - 
+// - - - - - -
 
-export async function writeRoomToCMS(worldAddress: string, roomID: string, prompt: string, imagePath: string) {
-    // Create a read stream from the image file
-    // const imageStream = createReadStream(imagePath);
-    
-    // Upload the image to Sanity using the file stream
-    // const imageAsset = await client.assets.upload('image', imageStream, {
-    //     filename: `room-${roomID}${imagePath.substring(imagePath.lastIndexOf('.'))}`,
-    //     contentType: 'image/png' // You might want to determine this based on the file extension
-    // });
+export async function writeRoomToCMS(
+  worldAddress: string,
+  roomID: string,
+  prompt: string,
+  imageBuffer: ReadableStream
+) {
+  const imageAsset = await client.assets.upload("image", imageBuffer, {
+    filename: `room-${roomID}.webp`,
+  })
 
-    // Create the room document with the uploaded image reference
-    const newRoomDoc = {
-        _type: "room",
-        title: roomID,
-        _id: roomID,
-        worldAddress: worldAddress,
-        prompt,
-        // image: {
-        //     _type: "image",
-        //     asset: {
-        //         _type: "reference",
-        //         _ref: imageAsset._id
-        //     }
-        // },
-        slug: {
-            _type: "slug",
-            current: roomID
-        }
-    };
+  // Create the room document with the uploaded image reference
+  const newRoomDoc = {
+    _type: "room",
+    title: roomID,
+    _id: roomID,
+    worldAddress: worldAddress,
+    prompt,
+    image: {
+      _type: "image",
+      asset: {
+        _type: "reference",
+        _ref: imageAsset._id,
+      },
+    },
+    slug: {
+      _type: "slug",
+      current: roomID,
+    },
+  }
 
-    // Create the room document in Sanity
-    const room = await client.create(newRoomDoc);
-    
-    return room;
+  // Create the room document in Sanity
+  const room = await client.create(newRoomDoc)
+
+  return room
 }
--- packages/server/src/routes/room/create/index.ts
+++ packages/server/src/routes/room/create/index.ts
@@ -1,78 +1,87 @@
-import { FastifyInstance, FastifyRequest } from 'fastify';
-import { schema } from '@routes/room/create/schema';
-import dotenv from 'dotenv';
+import { FastifyInstance, FastifyRequest } from "fastify"
+import { schema } from "@routes/room/create/schema"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
-import { MESSAGE } from '@config';
-import { CreateRoomBody } from '@routes/room/create/types';
+import { MESSAGE } from "@config"
+import { CreateRoomBody } from "@routes/room/create/types"
 
 // CMS
-import { writeRoomToCMS } from '@modules/cms';
+import { writeRoomToCMS } from "@modules/cms"
 
 // MUD
-import { systemCalls, network } from '@modules/mud/initMud';
+import { systemCalls, network } from "@modules/mud/initMud"
+
+// AI
+import { generateImage } from "@modules/image/generate"
 
 // Signature
-import { getSenderId } from '@modules/signature';
+import { getSenderId } from "@modules/signature"
 
 // Validate
-import { validateInputData } from './validation';
+import { validateInputData } from "./validation"
 
 // Error handling
-import { handleError } from './errorHandling';
+import { handleError } from "./errorHandling"
 
 // Utils
-import { generateRandomBytes32 } from '@modules/utils';
+import { generateRandomBytes32 } from "@modules/utils"
 
-const opts = { schema };  
+const opts = { schema }
 
-async function routes (fastify: FastifyInstance) {
-    fastify.post('/room/create', opts, async (request: FastifyRequest<{ Body: CreateRoomBody }>, reply) => {
+async function routes(fastify: FastifyInstance) {
+  fastify.post(
+    "/room/create",
+    opts,
+    async (request: FastifyRequest<{ Body: CreateRoomBody }>, reply) => {
+      try {
+        const { signature, roomName, roomPrompt } = request.body
+
+        // Recover player address from signature and convert to MUD bytes32 format
+        const playerId = getSenderId(signature, MESSAGE)
+
+        // TODO: Check if player has enough balance to create a room
+
+        // Check name and prompt lengths
+        validateInputData(roomName, roomPrompt)
+
+        // We need to generate a unique ID here
+        // Doing it onchain does not allow us to use it to connect the room to the image
+        const roomID = generateRandomBytes32()
+
+        // Create room onchain
+        console.time("–– Chain call")
+        await systemCalls.createRoom(playerId, roomID, roomName, roomPrompt)
+        console.timeEnd("–– Chain call")
+
+        // Generate image
+        console.time("–– Image generation")
+
+        // let imageAssetRef = null;
         try {
-            const {
-                signature,
-                roomName,
-                roomPrompt
-            } = request.body;
-            
-            // Recover player address from signature and convert to MUD bytes32 format
-            const playerId = getSenderId(signature, MESSAGE);
+          // Get the image data
+          const imageBuffer = await generateImage(roomPrompt)
+          // Get world addy
+          const worldAddress = network?.worldContract?.address ?? "0x0"
+          // Make the document
+          await writeRoomToCMS(worldAddress, roomID, roomPrompt, imageBuffer)
 
-            // TODO: Check if player has enough balance to create a room
-
-            // Check name and prompt lengths
-            validateInputData(roomName, roomPrompt);
-
-            // We need to generate a unique ID here
-            // Doing it onchain does not allow us to use it to connect the room to the image
-            const roomID = generateRandomBytes32(); 
-
-            // Create room onchain
-            console.time('–– Chain call');
-            await systemCalls.createRoom(playerId, roomID, roomName, roomPrompt);
-            console.timeEnd('–– Chain call');
-
-            // Generate image
-            console.time('–– Image generation');
-
-            // Call the image generation API
-            // Save in temp folder on server
-
-            // Include world address in the room doc so we can clear the database of unused rooms
-            const worldAddress = network?.worldContract?.address ?? "0x0";
-            // Upload to sanity
-            const newRoomDoc = await writeRoomToCMS(worldAddress, roomID, roomPrompt, ""); // no imagePath right now
-            console.log('newRoomDoc', newRoomDoc);
-            console.timeEnd('–– Image generation');
-
-            reply.send({
-                success: true,
-            })
+          console.log("Successfully created new room document!")
+          //
+          // imageAssetRef = newRoomDoc.image.asset._ref;
         } catch (error) {
-            return handleError(error, reply);
+          console.error("Error", error)
         }
-    });
+
+        reply.send({
+          success: true,
+        })
+      } catch (error) {
+        return handleError(error, reply)
+      }
+    }
+  )
 }
 
-export default routes;
+export default routes
