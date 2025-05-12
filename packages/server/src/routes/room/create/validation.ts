a5c34beb99cd200553e1d492fce9cd8051387430
PWR <pwrpntr@gmail.com>
2025-05-12 15:58:33 +0200

    change max room prompt length

--- packages/server/src/routes/room/create/validation.ts
+++ packages/server/src/routes/room/create/validation.ts
@@ -1,6 +1,6 @@
 export function validateInputData(roomPrompt: string) {
     // Check that the prompt is less than 1000 characters
-    if (roomPrompt.length < 1 || roomPrompt.length > 300) {
-        throw new Error('Room prompt must be between 1 and 300 characters.');
+    if (roomPrompt.length < 1 || roomPrompt.length > 280) {
+        throw new Error('Room prompt must be between 1 and 280 characters.');
     }
 }
