78979e9d05c203d62164d4c825d27c5c9ba5c0ba
PWR <pwrpntr@gmail.com>
2025-05-22 20:49:07 +0200

    switch to claude 4

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,7 +1,8 @@
 export const OFFCHAIN_VALIDATION_MESSAGE = "RATROOM"
 export const SANITY_ID = 'kupagww3'
 // export const ANTHROPIC_MODEL = "claude-3-5-haiku-latest"
-export const ANTHROPIC_MODEL = "claude-3-7-sonnet-latest"
+// export const ANTHROPIC_MODEL = "claude-3-7-sonnet-latest"
+export const ANTHROPIC_MODEL = "claude-sonnet-4-20250514"
 export const DEEPSEEK_MODEL = ""
 export const PORT = 3131;
 export const GAME_CONFIG_ID = "0x"
