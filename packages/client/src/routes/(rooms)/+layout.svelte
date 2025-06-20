82656bcb92bfeb238b2bda21e8fad7d37a20285a
PWR <pwrpntr@gmail.com>
2025-06-20 22:18:59 +0200

    wip accountkit

--- packages/client/src/routes/(rooms)/+layout.svelte
+++ packages/client/src/routes/(rooms)/+layout.svelte
@@ -1,45 +1,39 @@
 <script lang="ts">
-  import "../../app.css"
-  import "tippy.js/dist/tippy.css"
+	import '../../app.css';
+	import 'tippy.js/dist/tippy.css';
 
-  import type { LayoutProps } from './$types';
+	import type { LayoutProps } from './$types';
 
-  import Main from "$lib/components/Main/Main.svelte"
-  import { onMount } from "svelte"
-  import { initSound } from "$lib/modules/sound"
+	import Main from '$lib/components/Main/Main.svelte';
+	import { onMount } from 'svelte';
+	import { initSound } from '$lib/modules/sound';
 
-  let { children, data }: LayoutProps = $props()
+	let { children, data }: LayoutProps = $props();
 
+	onMount(async () => {
+		// Remove preloader
+		document.querySelector('.preloader')?.remove();
 
-  onMount(async () => {
-    // Remove preloader
-    document.querySelector(".preloader")?.remove()
+		// Preload sounds
+		initSound();
+	});
 
-    // Preload sounds
-    initSound()
-  })
-
-  $inspect(data.environment)
+	$inspect(data.environment);
 </script>
 
-
-
 <div class="main-area">
-  <Main environment={data.environment}>
-    {@render children?.()}
-  </Main>
+	<Main environment={data.environment}>
+		{@render children?.()}
+	</Main>
 </div>
 
 <style lang="scss">
-  .main-area {
-    width: 100%;
-    height: var(
-      --game-window-height
-    );
-    display: grid;
-    grid-template-columns: calc(var(--game-window-width) * 0.44) 1fr calc(
-        var(--game-window-width) * 0.44
-      );
-  }
+	.main-area {
+		width: 100%;
+		height: var(--game-window-height);
+		display: grid;
+		grid-template-columns: calc(var(--game-window-width) * 0.44) 1fr calc(
+				var(--game-window-width) * 0.44
+			);
+	}
 </style>
-
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,5 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson()],
+	define: {
+		// Fix for RAINBOW_PROVIDER_API_KEY error
+    	// https://github.com/rainbow-me/rainbowkit/discussions/2003
+		'process.env.RAINBOW_PROVIDER_API_KEY': JSON.stringify('')
+	}
 });
