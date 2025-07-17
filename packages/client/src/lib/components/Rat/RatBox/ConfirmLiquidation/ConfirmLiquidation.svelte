c03c1ebbd41161b9e78d138eaf9ddd5d7d76ad8b
PWR <pwrpntr@gmail.com>
2025-07-17 13:36:51 +0200

    client: work on RatBox

---
+++ packages/client/src/lib/components/Rat/RatBox/ConfirmLiquidation/ConfirmLiquidation.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { rat } from "$lib/modules/state/base/stores"
+  import { BigButton } from "$lib/components/Shared"
+  import { transitionTo, RAT_BOX_STATE } from "../state.svelte"
+
+  const onClickConfirm = () => {
+    // RAT_BOX_STATE.CONFIRM_LIQUIDATION -> RAT_BOX_STATE.LIQUIDATING_RAT
+    transitionTo(RAT_BOX_STATE.LIQUIDATING_RAT)
+  }
+
+  const onClickAbort = () => {
+    // RAT_BOX_STATE.CONFIRM_LIQUIDATION -> RAT_BOX_STATE.HAS_RAT
+    transitionTo(RAT_BOX_STATE.HAS_RAT)
+  }
+</script>
+
+<div class="confirm-liquidation">
+  <div class="confirm-liquidation-text">
+    <h1>Are you sure you want to liquidate {$rat?.name}?</h1>
+  </div>
+  <div class="confirm-liquidation-buttons">
+    <BigButton text="Confirm" onclick={onClickConfirm} />
+    <BigButton text="Abort" onclick={onClickAbort} />
+  </div>
+</div>
