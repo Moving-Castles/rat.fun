1e29a56c57bfaec3764884938c6a3dc33fddd86e
dk1a <dk1a@protonmail.com>
2025-07-17 14:26:01 +0200

    connectorClient

--- packages/client/src/lib/modules/entry-kit/SessionBridge.ts
+++ packages/client/src/lib/modules/entry-kit/SessionBridge.ts
@@ -1,14 +1,23 @@
 import { useEffect } from "react"
-import { useSessionClient } from "@latticexyz/entrykit/internal"
-import { entryKitSession } from "$lib/modules/entry-kit/stores"
+import { useEntryKitConfig, useSessionClient } from "@latticexyz/entrykit/internal"
+import { entryKitConnector, entryKitSession } from "$lib/modules/entry-kit/stores"
+import { useConnectorClient } from "wagmi"
 
 export default function SessionBridge() {
+  const { chainId } = useEntryKitConfig();
   const sessionClient = useSessionClient()
+
+  const connectorClient = useConnectorClient({ chainId })
 
   // Transfer session updates to svelte store
   useEffect(() => {
     entryKitSession.set(sessionClient.data)
-  }, [sessionClient.data, sessionClient])
+  }, [sessionClient.data])
+
+  // Transfer connector updates to svelte store
+  useEffect(() => {
+    entryKitConnector.set(connectorClient.data)
+  }, [connectorClient.data])
 
   return null
 }
