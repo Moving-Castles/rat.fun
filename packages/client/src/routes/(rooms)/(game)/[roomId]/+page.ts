215bbdffdc77c41ab277e3aa819270f9e866a2b1
manegame <contact@manusnijhoff.nl>
2025-07-25 11:15:59 +0200

    fix: revert to defaults for netlify

--- packages/client/src/routes/(rooms)/(game)/[roomId]/+page.ts
+++ packages/client/src/routes/(rooms)/(game)/[roomId]/+page.ts
@@ -4,8 +4,6 @@ import { queries } from "$lib/modules/content/sanity/groq"
 import { error, redirect } from "@sveltejs/kit"
 import { CMSError } from "$lib/modules/error-handling"
 
-export const ssr = false
-
 export const load: PageLoad = async ({ params }) => {
   try {
     console.log("room content getting loaded")
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -21,9 +21,6 @@ export default defineConfig(() => {
         }
       }
     ],
-    build: {
-      sourcemap: true
-    },
     ssr: {
       noExternal: [
         "@latticexyz/common",
