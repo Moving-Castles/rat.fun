ddc72911f69eac7e58371219038322b115532c67
PWR <pwrpntr@gmail.com>
2025-07-29 16:20:11 +0200

    contracts: work on inventory size

--- packages/contracts/src/constants.sol
+++ packages/contracts/src/constants.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.24;
 
 uint256 constant RAT_CREATION_COST = 100;
 
-uint32 constant MAX_INVENTORY_SIZE = 5;
+uint32 constant MAX_INVENTORY_SIZE = 6;
 uint32 constant MAX_ROOM_PROMPT_LENGTH = 280;
 uint32 constant MIN_ROOM_PROMPT_LENGTH = 10;
 
