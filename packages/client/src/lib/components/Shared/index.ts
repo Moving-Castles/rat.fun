9bf9f15e51079345f1414852e946aa269ed42a0b
PWR <pwrpntr@gmail.com>
2025-09-28 15:52:02 +0200

    client: work on simple shader

--- packages/client/src/lib/components/Shared/index.ts
+++ packages/client/src/lib/components/Shared/index.ts
@@ -12,6 +12,7 @@ export { default as Cross } from "./Graphics/Cross.svelte"
 export { default as Xed } from "./Graphics/Xed.svelte"
 export { default as Shader } from "./ShaderRenderer/Shader.svelte"
 export { default as ShaderRenderer } from "./ShaderRenderer/ShaderRenderer.svelte"
+export { default as SimpleShader } from "./ShaderRenderer/SimpleShader.svelte"
 
 /* Loaders */
 export { default as SmallSpinner } from "./Loaders/SmallSpinner.svelte"
