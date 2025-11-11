9a1ccba3f165c2d2b4122396aaaa4ff8e437ab20
PWR <pwrpntr@gmail.com>
2025-11-11 16:53:44 +0100

    entrykit: link in local package

--- packages/entrykit/tsup.config.ts
+++ packages/entrykit/tsup.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from "tsup";
-import { baseConfig } from "../../tsup.config.base";
-// import packageJson from "./package.json";
 
 export default defineConfig((opts) => ({
-  ...baseConfig(opts),
-  outDir: "dist/tsup",
   entry: ["src/exports/index.ts", "src/exports/internal.ts", "src/bin/deploy.ts"],
+  outDir: "dist/tsup",
+  format: ["esm"],
+  dts: false, // Disabled due to React version conflicts - will fix later
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  splitting: false,
+  treeshake: true,
+  target: "es2022",
+  external: [
+    "react",
+    "react-dom",
+    "viem",
+    "wagmi",
+    "@tanstack/react-query",
+  ],
   // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
-  // TODO: figure out how to do this conditionally for only specific imports?
   injectStyle: false,
   loader: { ".css": "text" },
+  esbuildOptions(options) {
+    options.jsx = "automatic";
+  },
 }));
