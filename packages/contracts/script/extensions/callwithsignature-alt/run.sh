f86c0ecff5d0f01d49bc51b5b658e0408e52ab72
PWR <pwrpntr@gmail.com>
2025-12-18 17:27:41 +0100

    contracts: CallWithSignatureAltModule deploy script for base-sepolia

--- packages/contracts/script/extensions/callwithsignature-alt/run.sh
+++ packages/contracts/script/extensions/callwithsignature-alt/run.sh
@@ -11,11 +11,21 @@
 # MAINNET (Base Mainnet)
 # # # # # # # # # # # #
 
-MAINNET_WORLD_ADDRESS=0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a
-MAINNET_RPC_URL=https://base-mainnet.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
+# MAINNET_WORLD_ADDRESS=0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a
+# MAINNET_RPC_URL=https://base-mainnet.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
 
 # forge script InstallCallWithSignatureAltModule --sig run\(address\) $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL --broadcast -vvv --batch-size 5
-forge script RegisterCallWithSignatureAltSystem --sig run\(address\) $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL --broadcast -vvv --batch-size 5
+# forge script RegisterCallWithSignatureAltSystem --sig run\(address\) $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL --broadcast -vvv --batch-size 5
+
+# # # # # # # # # # # #
+# BASE SEPOLIA
+# # # # # # # # # # # #
+
+BASE_SEPOLIA_WORLD_ADDRESS=0x85e4771925b8755f0F9Dd835aAa77303cbe9d684
+BASE_SEPOLIA_RPC_URL=https://base-sepolia.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
+
+forge script InstallCallWithSignatureAltModule --sig run\(address\) $BASE_SEPOLIA_WORLD_ADDRESS --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast -vvv --batch-size 5
+forge script RegisterCallWithSignatureAltSystem --sig run\(address\) $BASE_SEPOLIA_WORLD_ADDRESS --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast -vvv --batch-size 5
 
 # # # # # # # # # # # #
 # DEBUG SIGNATURE
