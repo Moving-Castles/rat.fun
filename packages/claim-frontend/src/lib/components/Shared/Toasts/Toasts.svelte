868d7d29b40f824173cb6e1a348b68dbffb7a418
manegame <contact@manusnijhoff.nl>
2026-01-22 17:55:05 +0100

    fix: add back in auction and claim folders

---
+++ packages/claim-frontend/src/lib/components/Shared/Toasts/Toasts.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Toasts as BaseToasts } from "@ratfun/shared-ui/Toasts"
+  import { toastManager } from "$lib/modules/ui/toasts.svelte"
+
+  const onRemove = (id: string) => {
+    toastManager.remove(id)
+  }
+</script>
+
+<BaseToasts toasts={toastManager.toasts} {onRemove} />
